5a11477e056ce4de1ed505c12e68d4a8b79d0243
Medha Mishra <mmisra2365@gmail.com>
2023-12-18 20:58:11 +0530

    second commit

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+window.onload = function() {
+    document.getElementById("day").value = "";
+    document.getElementById("month").value = "";
+    document.getElementById("year").value = "";
+
+}
 var inputs=document.querySelectorAll(".time");
-var currTime=new Date();
+var today=new Date();
 for(var i=0;i<document.querySelectorAll(".time").length;i++){
     inputs[i].addEventListener("change", function(){
-        const Id = this.id;
-        const funcionName = "time"+Id;
-        
-        
+        const Id=this.id;
+        const functionName="time"+Id;
+        window[functionName]();
     });
 }
 function timeday(){
     var date = document.getElementById("day").value;
     var dateError = document.getElementById("day-invalid");
-
-    if(date <= 0 || date > 31){
+   
+    if(date <= 0 || date > 31 ){
         dateError.classList.remove("hidden");
     }
     else{
@@ -20,9 +25,9 @@ function timeday(){
     }
 }
 function timemonth(){
-    var month = document.getElementById("month").value;
+     var month = document.getElementById("month").value;
     var monError = document.getElementById("month-invalid");
-
+    
     if(month <=0 || month > 12){
         monError.classList.remove("hidden");
     }
@@ -33,8 +38,8 @@ function timemonth(){
 function timeyear(){
     var year = document.getElementById("year").value;
     var yearError = document.getElementById("year-invalid");
-
-    if( year > 2023){
+    
+    if( year< 1950 || year > 2023){
         yearError.classList.remove("hidden");
     }
     else{
@@ -42,13 +47,49 @@ function timeyear(){
     }
 }
 
-document.getElementById("submit-btn").addEventListener("click", function(){
+
+var date = document.getElementById("day").value;
+var dateError = document.getElementById("day-invalid");
+
+var month = document.getElementById("month").value;
+var monthError = document.getElementById("month-invalid");
+
+var year = document.getElementById("year").value;
+var yearError = document.getElementById("year-invalid");
+  
+
+document.getElementById("submit-btn").addEventListener("click", function handleClick(event){
+    if(date==="" || month===""||year===""){
+        dateError.classList.remove("hidden");
+        monthError.classList.remove("hidden");
+        yearError.classList.remove("hidden");
+    }
+  
+    
+  else{
     calculateAge(document.getElementById("year").value, document.getElementById("month").value, document.getElementById("day").value);
+
+    // event.preventDefault();
+    // const Input = document.querySelectorAll("#day,#month,#year");
+
+    // Input.forEach(input => {
+    //   input.value = '';
+    // });
+  }
+   
 });
 
-
+  
 function calculateAge(year,month,day){
-
+    var past =new Date(year,month-1,day);
+    var ageDifference = today -past;
+    var ageDate = new Date(ageDifference);
+    var years = ageDate.getFullYear()-1970;
+    var months = ageDate.getMonth();
+    var days = ageDate.getDate()-1;
+    document.getElementById("year-smt").innerHTML = years;
+    document.getElementById("month-smt").innerHTML = months;
+    document.getElementById("day-smt").innerHTML  = days;
 }
 
 
@@ -104,4 +145,3 @@ function calculateAge(year,month,day){
 
 
 
-window[funcionName]();
